01a570c7a8c2e1a00eec202219f3f67a182104f2
hieuleader <hieuleadergin@gmail.com>
2019-03-22 02:00:58 +0700

    Add xethron

--- config/app.php
+++ config/app.php
@@ -163,6 +163,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Way\Generators\GeneratorsServiceProvider::class,
+        Xethron\MigrationsGenerator\MigrationsGeneratorServiceProvider::class,
 
         /*
          * Package Service Providers...
