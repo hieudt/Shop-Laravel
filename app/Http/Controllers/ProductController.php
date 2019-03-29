1cfec62ad138214243ca6508e9ca1857ae57834b
hieuleader <hieuleadergin@gmail.com>
2019-03-29 22:20:55 +0700

    Edit sidebar

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        return view('admin.product.list');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('check',function(){
 
 Route::group(['prefix' => 'admin'], function () {
     //Voyager::routes();
-    Route::get('/','AdminPages@index');
+    Route::get('/index','AdminPages@index')->name('admin.index');
 
     Route::get('category','CategoryController@index')->name('category.list');
     Route::get('category/Search','CategoryController@Search')->name('category.search');
@@ -35,6 +35,8 @@ Route::group(['prefix' => 'admin'], function () {
     Route::post('subcategory/Store','SubCategoryController@store')->name('subcategory.store');
     Route::post('subcategory/update','SubCategoryController@update')->name('subcategory.update');
     Route::get('subcategory/delete/{id}','SubCategoryController@destroy')->name('subcategory.destroy');
+
+    Route::get('product','ProductController@index')->name('product.list');
 });
 
 
