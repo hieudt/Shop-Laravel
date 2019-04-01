e64e783568c0a2b6f6bcabf8c2588d5ba718e26a
hieuleader <hieuleadergin@gmail.com>
2019-04-01 18:45:48 +0700

    Updated path

--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,7 @@ Route::group(['prefix' => 'admin'], function () {
     
     Route::get('size/Search','SizeController@search')->name('size.search');
 
-    Route::post('productdetails','ProductDetailsController@test')->name('productdetails.store');
+    Route::post('productdetails','ProductDetailsController@store')->name('productdetails.store');
 
     Route::get('getapi/minmax/{min}/{max}','ApiController@minmax')->name('getapi.minmax');
     Route::get('getapi/service/{msg}','ApiController@service')->name('getapi.service');
