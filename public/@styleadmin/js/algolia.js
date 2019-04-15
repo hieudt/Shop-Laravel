dfc5103b6fe4971cb49b32d238067f56b51e8b8d
hieuleader <hieuleadergin@gmail.com>
2019-04-15 22:03:40 +0700

    Search Bar

--- public/@styleadmin/js/algolia.js
+++ public/@styleadmin/js/algolia.js
@@ -1,14 +1,35 @@
-var client = algoliasearch('CI2EIBUHHH', 'acf91bc5e3367faf4c70e87dfc2ef866');
-var index = client.initIndex('contacts');
-autocomplete('#search-input', { hint: false }, 
-{
-    source: autocomplete.sources.hits(index, { hitsPerPage: 5 }),
-    displayKey: 'my_attribute',
-    templates: {
-        suggestion: function(suggestion) {
-            return '<span>'+
-                suggestion._highlightResult.my_attribute.value
-                +'</span>';
-        }
-    }
-});
+
+(function() {
+    var client = algoliasearch('CI2EIBUHHH', 'acf91bc5e3367faf4c70e87dfc2ef866');
+    var index = client.initIndex('Product');    
+    var enterPressed = false;
+    //initialize autocomplete on search input (ID selector must match)
+    autocomplete('#aa-search-input',
+        { hint: false }, {
+            source: autocomplete.sources.hits(index, { hitsPerPage: 10 }),
+            //value to be displayed in input control after user's suggestion selection
+            displayKey: 'title',
+            //hash of templates used when rendering dataset
+            templates: {
+                //'suggestion' templating function used to render a single suggestion
+                suggestion: function (suggestion) {
+                    const markup = `
+                        <div class="algolia-result">
+                            <span>
+                            <img src="${window.location.origin}/images/product/${suggestion.thumbnail}" alt="img" class="algolia-thumb">
+                                <span class="aa-title">${suggestion.title} | ${suggestion.cost} VNĐ</span>
+                            </span>
+                            
+                        </div>
+                    `;
+                    return markup;
+                },
+                empty: function (result) {
+                    return 'Không tìm thấy kết quả nào cho "' + result.query + '"';
+                }
+            }
+        }).on('autocomplete:selected', function (event, suggestion, dataset) {
+            window.location.href = window.location.origin + '/san-pham/'+suggestion.id+'/'+ suggestion.slug;
+            enterPressed = true;
+        });
+})();
--- resources/views/includes/master.blade.php
+++ resources/views/includes/master.blade.php
@@ -28,6 +28,7 @@
     <link href="{{ URL::asset('assets/css/genius-gallery.css')}}" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('@styleadmin/node_modules/jquery-toast-plugin/dist/jquery.toast.min.css')}}">
     <link href="{{ URL::asset('assets/css/lightbox.css')}}" rel="stylesheet">
+    <link href="{{ URL::asset('@styleadmin/css/algolia.css')}}" rel="stylesheet">
     <link href="{{ URL::asset('assets/css/animate.min.css')}}" rel="stylesheet">
 
     <!-- Inject CSS -->
@@ -48,14 +49,13 @@
     @include('includes.footer')
     @include('includes.cart')
         <div class="search-box popup">
-            <form id="searchform">
-                <button type="button" id="searchbtn" class="search-button">
-                <i class="fa fa-search"></i>
-            </button>
-                <div class="search-field">
-                    <input type="text" id="searchdata" value="" placeholder="Search for product" />
-                </div>
-            </form>
+            <div class="aa-input-container" id="aa-input-container">
+                <input type="search" id="aa-search-input" class="aa-input-search" placeholder="Tìm kiếm sản phẩm" name="search"
+                    autocomplete="off" />
+                <svg class="aa-input-icon" viewBox="654 -372 1664 1664">
+                    <path d="M1806,332c0-123.3-43.8-228.8-131.5-316.5C1586.8-72.2,1481.3-116,1358-116s-228.8,43.8-316.5,131.5  C953.8,103.2,910,208.7,910,332s43.8,228.8,131.5,316.5C1129.2,736.2,1234.7,780,1358,780s228.8-43.8,316.5-131.5  C1762.2,560.8,1806,455.3,1806,332z M2318,1164c0,34.7-12.7,64.7-38,90s-55.3,38-90,38c-36,0-66-12.7-90-38l-343-342  c-119.3,82.7-252.3,124-399,124c-95.3,0-186.5-18.5-273.5-55.5s-162-87-225-150s-113-138-150-225S654,427.3,654,332  s18.5-186.5,55.5-273.5s87-162,150-225s138-113,225-150S1262.7-372,1358-372s186.5,18.5,273.5,55.5s162,87,225,150s113,138,150,225  S2062,236.7,2062,332c0,146.7-41.3,279.7-124,399l343,343C2305.7,1098.7,2318,1128.7,2318,1164z" />
+                </svg>
+            </div>
         </div>
     </div>
     <script>
@@ -79,10 +79,10 @@
     <!-- custom scrollbar -->
     <script src="{{asset('@styleadmin/node_modules/jquery-toast-plugin/dist/jquery.toast.min.js')}}"></script>
     <script src="{{asset('@styleadmin/js/toastDemo.js')}}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/algoliasearch@3.32.0"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-autocomplete/1.0.7/jquery.auto-complete.min.js"></script>
-    <script src="{{asset('js/algolia.js')}}"></script>
-    
+    <script src="https://cdn.jsdelivr.net/algoliasearch/3/algoliasearch.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/autocomplete.js/0/autocomplete.min.js"></script>
+    <script src="{{asset('@styleadmin/js/algolia.js')}}"></script>
+
     
     <script src="{{asset('@styleadmin/js/pusher.min.js')}}"></script>
     <script src="{{asset('@styleadmin/js/myjs.js')}}"></script>
@@ -101,23 +101,7 @@
             },500)
             loadCart();
         });
-        $("#searchbtn").click(function (){
-        if($("#searchdata").val() != ""){
-                    window.location = "{{url('/')}}/search/"+$("#searchdata").val();
-                }else{
-                    window.location = "{{url('/')}}/search/none";
-                }
-    	});
-	    $("#searchdata").keypress(function(event) {
-	        if (event.which == 13) {
-	            event.preventDefault();
-	            if($("#searchdata").val() != ""){
-	            	window.location = "{{url('/')}}/search/"+$("#searchdata").val();
-	            }else{
-	            	window.location = "{{url('/')}}/search/none";
-	            }
-	        }
-	    });
+
 
 
     $("#LoginButton").click(function(){
