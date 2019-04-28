42909f4139fd64c580e4ce96c87457a6f385875f
hieuleader <hieuleadergin@gmail.com>
2019-04-28 17:13:05 +0700

    Location Pack

--- app/Http/Controllers/SafeModeController.php
+++ app/Http/Controllers/SafeModeController.php
@@ -8,8 +8,9 @@ use phpDocumentor\Reflection\Location;
 class SafeModeController extends Controller
 {
     public function AlertLogin(Request $req){
-        echo $req->server('HTTP_USER_AGENT');
-        echo $req->ip();
+        echo $req->server('HTTP_USER_AGENT')."<br/>";
+        $ip = trim(shell_exec("dig +short myip.opendns.com @resolver1.opendns.com"));
+        echo $ip."<br/>";
         echo Location::get($req->ip());
     }
 }
